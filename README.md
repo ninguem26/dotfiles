3f200d6c8b30be373b5bf5d9735461b548ec1902
Júlio César <diolandajr@gmail.com>
2020-03-21 22:53:56 -0300

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# dotfiles
-i3wm dotfiles
+# Dotfiles
+My i3wm dotfiles
+
+## Installation
+
+To configure i3wm you just need to run the `install.sh` file in the command line:
+> ~$ ./install.sh
+
+This will install all the libs necessary to make the i3 environment work.
+
+### Dunst
+
+For notifications to work, install the Dunst notification daemon, following the [docs](https://github.com/dunst-project/dunst).
+
+### Light
+
+For screen back light control, install [Light](http://haikarainen.github.io/light/).
+
+## Other tips
+
+- Media control can present some problems if the output device code is diferent from the one in i3/config. If that happens, just change the code in it's respective lines.
--- install.sh
+++ install.sh
@@ -2,15 +2,17 @@
 
 echo "Preparing to install packages..."
 
-apt-get update
-
 echo "Adding repository PPAs"
 
 sudo add-apt-repository ppa:apandada1/brightness-controller
 
+echo "Updating repositories"
+
+sudo apt-get update
+
 echo "Installing..."
 
-apt-get install i3 i3-wm i3status i3blocks i3lock gedit dmenu lxappearance thunar pavucontrol rofi xbacklight compton brightness-controller acpi sysstat arc-theme fonts-powerline rxvt-unicode zsh tilix vim maim xclip feh arandr ranger w3m
+sudo apt-get install i3 i3-wm i3status i3blocks i3lock gedit dmenu lxappearance thunar pavucontrol rofi xbacklight compton brightness-controller acpi sysstat arc-theme fonts-powerline rxvt-unicode zsh tilix vim maim xclip feh arandr ranger w3m
 
 echo "Pacotes instalados com sucesso!"
 
